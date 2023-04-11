84eafaa38fe3f93cd589ec67ff51c17de9633ad8
@yuri_assuncx <killerkoll2012@gmail.com>
2023-04-11 16:37:31 -0300

    fix: bugs fixed 🚀

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -17,7 +17,7 @@ import { BsSnow } from 'react-icons/bs';
 import { IoDiamond } from 'react-icons/io5';
 import { MdOutlineVilla } from 'react-icons/md';
 
-import { CategoryBox } from '../CategoryBox';
+import CategoryBox from '../CategoryBox';
 
 export const categories = [
   {
